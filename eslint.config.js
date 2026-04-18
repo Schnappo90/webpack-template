d386f11cb04b8f35de0a4f27dd0dbeb424c36578
Schnappo90 <alexwilliamknapp@gmail.com>
2026-04-18 12:22:01 +0100

    Add template files

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+]);
